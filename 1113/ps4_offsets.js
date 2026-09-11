d4aa64b3f2395dbdc9061925b628134e43ddf3cc
Ibrahim Yusuf <123777666+B4411M@users.noreply.github.com>
2026-09-11 22:54:49 +0800

    Update PS4 offsets for firmware 13.02

--- 1113/ps4_offsets.js
+++ 1113/ps4_offsets.js
@@ -454,12 +454,13 @@ PS4["12.52"] = Object.assign({}, PS4["12.50"], {
     kpatch: "1250.bin",
 });
 
-PS4["13.02"] = Object.assign({}, PS4["12.00"], {
-    alias_of: "12.00",
-    fw_status: "state=UNTESTED-on-hardware shares=12.00 "
-        + "kernel_rvas=verified-vs-kernel_1302.elf (this firmware) "
-        + "kpatch=1200.bin-10-sites-verified bug=lapse",
-    kpatch: "1200.bin",
+PS4["13.02"] = Object.assign({}, PS4["13.00"], {
+    alias_of: "13.00",
+    fw_status: "state=UNTESTED-on-hardware shares=13.00 "
+        + "webkit=assumed-identical-to-13.00 (no 13.02 module dump) "
+        + "kernel_rvas=asserted-by-supplied-table UNVERIFIED "
+        + "kpatch=1300.bin bug=poops",
+    kpatch: "1300.bin",
 });
 
 PS4["13.04"] = Object.assign({}, PS4["13.00"], {
